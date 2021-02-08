dc606986169cb3d70c6baae0818c4d3ed91104fd
Aga <agnieszkajureczko@o2.pl>
2021-02-08 11:49:32 +0100

    adding behave.ini

--- features/fixtures.py
+++ features/fixtures.py
@@ -4,7 +4,7 @@ from behave.fixture import fixture
 
 @fixture
 def browser_chrome(context):
-    context.driver = webdriver.Chrome("C:/drivers/chromedriver.exe")
+    context.driver = webdriver.Chrome(context.config.userdata['driver'])
     context.driver.implicitly_wait(10)
     yield context.driver
     context.driver.quit()
